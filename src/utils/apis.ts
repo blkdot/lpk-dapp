b76c253ec358b5a458381c9f3aca8872ebeb0c48
Sam <snoblefuentes@gmail.com>
2021-11-27 05:08:23 +0900

    update api

--- src/utils/apis.ts
+++ src/utils/apis.ts
@@ -1,4 +1,4 @@
-  export const BASE_ENDPOINT = 'http://127.0.0.1:8000'
+  export const BASE_ENDPOINT = 'https://vast-ravine-02476.herokuapp.com'
   export const SERVER_ENDPOINT = `${BASE_ENDPOINT}/api/v1`
 
   export const MARKETS_ENDPOINT = `${SERVER_ENDPOINT}/markets`
